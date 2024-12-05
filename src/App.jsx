dec00b8c99f41e4915d0674df28fa7a9a83af917
ianmarinovski <144111969+DegenYan@users.noreply.github.com>
2024-12-05 15:35:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import React, { useEffect, createContext } from "react";
+import React, { createContext } from "react";
 
 import Home from "./pages/home.jsx";
 
