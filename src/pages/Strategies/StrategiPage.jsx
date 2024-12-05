d04fad1a7c5ba44dd560de26dbf93c80ce32b2cb
ianmarinovski <144111969+DegenYan@users.noreply.github.com>
2024-12-05 15:52:47 +0200

    Fix file name case issues

---
+++ src/pages/Strategies/StrategiPage.jsx
@@ -0,0 +1,57 @@
+import "../../css/safe.css";
+import Header from "../../componets/UI/header";
+import HowWeWork from "./howWeWork";
+import Feedback from "../../componets/UI/feedback";
+import Footer from "../../componets/UI/footer";
+import React, { useEffect } from 'react';
+import { DAOs_DATA, DAOs_PORTFOLIOS, STRATEGI_KEYS } from "../../constants/strategis";
+import { useDaoPrice } from "../../hooks/useDaoPrice";
+import { useStore } from "../../hooks/useStore";
+import Portfolio from "./Portfolio_";
+
+const StrategiPage = ({ dao, daoKey }) => {
+
+    const store = useStore()
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Плавная прокрутка
+        });
+    }, [dao]);
+    const daoData = DAOs_DATA[dao]
+    const { daoPrice, tokens } = useDaoPrice(daoData.currentDaoAddress, daoData.lpAddress, daoData.chainId)
+
+    const portfolio = DAOs_PORTFOLIOS[dao] ?? tokens ?? undefined
+
+    // const { res, } = useByBitCoinsBalance()
+    // console.log(res)
+
+    useEffect(() => {
+        if (portfolio !== undefined && portfolio[0]?.price !== undefined) {
+            if (dao === STRATEGI_KEYS.btcdao && portfolio[0]?.price) {
+                store.setBtcRate(portfolio[0].price)
+            }
+            store.setPortfolio(dao, tokens)
+        }
+    }, [tokens])
+
+    useEffect(() => {
+        if (daoPrice !== undefined) store.setDaoPrice(daoKey, daoPrice)
+
+    }, [daoPrice])
+
+    // console.log(tokens)
+    return (
+        <div className="main" key={daoKey + "swap"}>
+            <Header />
+            <HowWeWork dao={dao} daoKey={daoKey} daoPrice={daoPrice ?? store.daoPrices[daoKey]} />
+            <Portfolio portfolio={portfolio ?? store.daoPortfolios[daoKey]
+            } dao={dao} />
+            <Feedback />
+            <Footer />
+        </div>
+    );
+};
+
+export default StrategiPage;
