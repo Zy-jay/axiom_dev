f6ff74bf6ab03b26452d110599f61d8fd34d6301
ianmarinovski <144111969+DegenYan@users.noreply.github.com>
2024-12-05 16:08:41 +0200

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,30 @@
 const webpack = require("webpack");
 
-module.exports = {
-  webpack: {
-    configure: {
-      resolve: {
-        fallback: {
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify"),
-          assert: require.resolve("assert"),
-          http: require.resolve("stream-http"),
-          https: require.resolve("https-browserify"),
-          os: require.resolve("os-browserify/browser"),
-          buffer: require.resolve("buffer"),
-        },
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Buffer: ["buffer", "Buffer"],
-          process: "process/browser",
-        }),
-      ],
-    },
-  },
+module.exports = function override(config) {
+	const fallback = config.resolve.fallback || {};
+	Object.assign(fallback, {
+		crypto: require.resolve("crypto-browserify"),
+		stream: require.resolve("stream-browserify"),
+		assert: require.resolve("assert"),
+		http: require.resolve("stream-http"),
+		https: require.resolve("https-browserify"),
+		os: require.resolve("os-browserify"),
+		url: require.resolve("url"),
+		process: require.resolve("process/browser"), // Ensure this fallback
+	});
+	config.resolve.fallback = fallback;
+	config.plugins = (config.plugins || []).concat([
+		new webpack.ProvidePlugin({
+			process: "process/browser",
+			Buffer: ["buffer", "Buffer"],
+		}),
+	]);
+	config.module.rules.unshift({
+		test: /\.m?js$/,
+		resolve: {
+			fullySpecified: false, // disable the behavior
+		},
+	});
+
+	return config;
 };
